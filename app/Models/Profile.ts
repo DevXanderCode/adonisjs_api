4b2c0c62d5761e911cb812c12b2414cec5e8c837
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-08-22 06:32:55 +0100

    installed and configured attachment-lite

--- app/Models/Profile.ts
+++ app/Models/Profile.ts
@@ -1,9 +1,19 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { attachment, AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
 
 export default class Profile extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public user_id: number
+
+  @column()
+  public biography: string
+
+  @attachment()
+  public avatar: AttachmentContract
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
